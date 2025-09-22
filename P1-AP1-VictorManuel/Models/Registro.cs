8f7a236e9c075b598b5a21d38e90f8e7fd565ec7
Victor Manuel Frias De Jesús <victor_frias1@ucne.edu.do>
2025-09-22 19:21:43 -0400

    Models agregado con el Id unicamente

---
+++ P1-AP1-VictorManuel/Models/Registro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_VictorManuel.Models;
+
+
+    public class Registro
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+  
